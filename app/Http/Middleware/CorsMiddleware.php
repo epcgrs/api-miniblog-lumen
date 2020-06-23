30133e34ecf5a9f592ba48d5697b9acb12943dd4
Emmanuel <emmanuelf988@gmail.com>
2020-06-23 20:24:32 -0300

    fix cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,29 +17,11 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment() === 'production') {
-            return redirect()->to($request->getRequestUri(), 302, [], true);
-        }
 
-        $headers = [
-            'Access-Control-Allow-Origin'      => 'https://miniblog-epcg.netlify.app',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
-        ];
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
 
-        if ($request->isMethod('OPTIONS'))
-        {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
-
-        return $response;
     }
 }
