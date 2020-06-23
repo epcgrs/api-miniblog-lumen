c7e474d18578e59dd1051b8284923aa14543e59f
Emmanuel <emmanuelf988@gmail.com>
2020-06-23 20:35:51 -0300

    fix cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,12 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-
+        if ($request->getMethod() === "OPTIONS") {
+            return response('')
+                ->header('Access-Control-Allow-Origin', 'https://miniblog-epcg.netlify.app')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');;
+        }
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'https://miniblog-epcg.netlify.app')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
