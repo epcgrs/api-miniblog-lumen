32747976439e8f099a36a968980bb4e87d9cfe57
Emmanuel <emmanuelf988@gmail.com>
2020-06-23 19:07:41 -0300

    update request

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,12 +17,22 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!$request->secure() && app()->environment() === 'production') {
+            return redirect()->to($request->getRequestUri(), 302, [], true);
+        }
 
         $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, PUT, DELETE',
+            'Access-Control-Allow-Origin'      => 'https://miniblog-epcg.netlify.app',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
+
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
 
         $response = $next($request);
         foreach($headers as $key => $value)
